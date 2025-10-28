27e02351f2adc7099f5d2ae90715a9536cffd1f6
David Bonilla <thegrindnet@gmail.com>
2025-10-27 22:56:50 -0600

    Connect Mongoose

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 // On Video 4: Mongoose Connection
 const express = require("express");
+const mongoose = require("mongoose");
 const { PORT = 3001 } = process.env;
 const app = express();
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
