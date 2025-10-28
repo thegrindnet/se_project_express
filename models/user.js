e33b349213f9643f762b06a16c99e65798ce0ec0
David Bonilla <thegrindnet@gmail.com>
2025-10-27 23:13:39 -0600

    Add validator to user.js in models directory and make shell for clothingItem.js in same directory

---
+++ models/user.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true, minlength: 2, maxlength: 30 },
+  avatar: {
+    type: String,
+    required: [true, "The avatar field is required"],
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: "You must enter a valid URL",
+    },
+  },
+});
+
+module.exports = mongoose.model("user", userSchema);
