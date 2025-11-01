b9883467c29e1464b100bc74a197396afec6aed7
David Bonilla <thegrindnet@gmail.com>
2025-10-31 23:03:40 -0600

    Remove app.use with mainRoute index.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const mainRouter = require("./routes/index");
 
 const { PORT = 3001 } = process.env;
 const app = express();
@@ -25,7 +24,6 @@ const routes = require("./routes");
 
 app.use(express.json());
 app.use("/", routes);
-// app.use("/", mainRouter);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
