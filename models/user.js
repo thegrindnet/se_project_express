cfbe117816bf3b029e37b10b16978e2e51dbd12b
David Bonilla <thegrindnet@gmail.com>
2025-11-25 00:45:18 -0700

    Update user.js Schema to findUserByCredentials

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
-// const bcrypt = require("bcryptjs");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true, minlength: 2, maxlength: 30 },
@@ -32,25 +32,25 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-// userSchema.statics.findUserByCredentials = function findUserByCredentials(
-//   email,
-//   password
-// ) {
-//   return this.findOne({ email })
-//     .select("+password")
-//     .then((user) => {
-//       if (!user) {
-//         return Promise.reject(new Error("Incorrect email or password"));
-//       }
+userSchema.statics.findUserByCredentials = function findUserByCredentials(
+  email,
+  password
+) {
+  return this.findOne({ email })
+    .select("+password")
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error("Incorrect email or password"));
+      }
 
-//       return bcrypt.compare(password, user.password).then((matched) => {
-//         if (!matched) {
-//           return Promise.reject(new Error("Incorrect email or password"));
-//         }
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(new Error("Incorrect email or password"));
+        }
 
-//         return user;
-//       });
-//     });
-// };
+        return user;
+      });
+    });
+};
 
 module.exports = mongoose.model("user", userSchema);
