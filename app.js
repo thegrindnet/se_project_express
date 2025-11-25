a47d11ad71d2cffad263de3b7b53132403ea7a60
David Bonilla <thegrindnet@gmail.com>
2025-11-24 22:39:50 -0700

    Create middleware directory with the auth.js file

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const { PORT = 3001 } = process.env;
 const app = express();
+
+const mainRouter = require("./routes/index");
+const { createUser, login } = require("./controllers/users");
+
+const { PORT = 3001 } = process.env;
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
@@ -20,10 +24,14 @@ app.use((req, res, next) => {
   next();
 });
 
-const routes = require("./routes");
+// const routes = require("./routes");
 
 app.use(express.json());
-app.use("/", routes);
+
+app.post("/signin", login);
+app.post("/signup", createUser);
+
+app.use("/", mainRouter);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
