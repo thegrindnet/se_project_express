0926e4310d14d437ada0c62ad8a56ef076a96c8f
David Bonilla <thegrindnet@gmail.com>
2026-02-04 01:07:11 -0700

    Update index.js in Routes directory to support Centralized error handling, create validation.js in Middleware and add clothing and user validation

---
+++ middlewares/validation.js
@@ -0,0 +1,50 @@
+const { Joi, celebrate } = require("celebrate");
+const validator = require("validator");
+
+const validateURL = (value, helpers) => {
+  if (validator.isURL(value)) {
+    return value;
+  }
+  return helpers.error("string.uri");
+};
+
+const clothingItemBodyValidation = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30).messages({
+      "string.empty": "Name is required",
+      "string.min": "Name must be at least 2 characters long",
+      "string.max": "Name must be at most 30 characters long",
+    }),
+
+    imageUrl: Joi.string().required().custom(validateURL).messages({
+      "string.empty": "Image URL is required",
+      "string.uri": "Image URL must be a valid URL",
+    }),
+  }),
+});
+
+const userInfoBodyValidation = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30).messages({
+      "string.empty": "Name is required",
+      "string.min": "Name must be at least 2 characters long",
+      "string.max": "Name must be at most 30 characters long",
+    }),
+    avatar: Joi.string().required().custom(validateURL).messages({
+      "string.empty": "Avatar URL is required",
+      "string.uri": "Avatar URL must be a valid URL",
+    }),
+    email: Joi.string().required().email().messages({
+      "string.empty": "Email is required",
+      "string.email": "Email must be a valid email address",
+    }),
+    password: Joi.string().required().messages({
+      "string.empty": "Password is required",
+    }),
+  }),
+});
+
+module.exports = {
+  clothingItemBodyValidation,
+  userInfoBodyValidation,
+};
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
+const { NotFoundError } = require("../utils/errors/notfound-error");
 
 const userRouter = require("./users");
 const clothingItem = require("./clothingItems");
 // const auth = require("../middlewares/auth");
 
-const { NOT_FOUND } = require("../utils/errors");
+// const { NOT_FOUND } = require("../utils/errors");
 
 // router.use(auth);
 
@@ -12,8 +13,9 @@ router.use("/items", clothingItem);
 
 router.use("/users", userRouter);
 
-router.use((req, res) => {
-  res.status(NOT_FOUND).send({ message: "Router resource not found" });
+router.use((req, res, next) => {
+  next(new NotFoundError("Router resource not found"));
+  // res.status(NOT_FOUND).send({ message: "Router resource not found" });
 });
 
 module.exports = router;
