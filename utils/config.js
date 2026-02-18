12128d699d8ab524ec23fc8759d576edfefbba85
David Bonilla <thegrindnet@gmail.com>
2026-02-17 23:00:23 -0700

    In app.js required dotenv and changed config.js JWT_SECRET

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,6 @@
-const JWT_SECRET =
-  "7adad02fa5f1bc93624b08c0c8146fc0bb16ac7388fcdc0f37de6e1ed95430c7def153fdc842d2e0915e9319f9ef4eff82ffa572c261ab47ad2f063a5cc472d0";
+const {
+  JWT_SECRET = "7adad02fa5f1bc93624b08c0c8146fc0bb16ac7388fcdc0f37de6e1ed95430c7def153fdc842d2e0915e9319f9ef4eff82ffa572c261ab47ad2f063a5cc472d0",
+} = process.env;
 
 module.exports = {
   JWT_SECRET,
